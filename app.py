7ee6329096944237f3c5f7eb36413d0e958d0571
Gideon Tettehfio <gideonlambride@gmail.com>
2025-04-18 15:18:24 +0000

    localhost mysql for database and s3 for media storage

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ from datetime import datetime
 import os
 from dotenv import load_dotenv
 
+import boto3
+from botocore.exceptions import NoCredentialsError
+from werkzeug.utils import secure_filename
+import uuid
+
+
 app = Flask(__name__)
 
 
@@ -21,6 +27,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Suppress warning
 # Initialize Database
 db = SQLAlchemy(app)
 
+# Configure S3 client
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv('AWS_ACCESS_KEY'),
+    aws_secret_access_key=os.getenv('AWS_SECRET_KEY'),
+    region_name=os.getenv('AWS_REGION')
+)
 
 class BlogPost(db.Model):
     __tablename__ = 'BlogPost'
@@ -29,6 +42,7 @@ class BlogPost(db.Model):
     content = db.Column(db.Text, nullable=False)
     author = db.Column(db.String(20), nullable=False, default='N/A')
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    cover_image_url = db.Column(db.String(200))
 
     def __repr__(self):
         return 'Blog post ' + str(self.id)
@@ -45,7 +59,34 @@ def posts():
         post_title = request.form['title']
         post_content = request.form['content']
         post_author = request.form['author']
-        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        cover_image = request.files.get('cover_image')  # Get the uploaded file
+        cover_image_url = None
+
+        if cover_image:
+            try:
+                # Generate a unique filename
+                filename = f"{uuid.uuid4()}_{secure_filename(cover_image.filename)}"
+                # Upload to S3
+                s3.upload_fileobj(
+                    cover_image,
+                    os.getenv('S3_BUCKET_NAME'),
+                    filename,
+                    ExtraArgs={'ContentType': cover_image.content_type}
+                )
+                # Generate the public URL
+                cover_image_url = f"https://{os.getenv('S3_BUCKET_NAME')}.s3.amazonaws.com/{filename}"
+            except NoCredentialsError:
+                return "AWS credentials not found", 400
+            except Exception as e:
+                return str(e), 500
+
+        new_post = BlogPost(
+            title=post_title,
+            content=post_content,
+            author=post_author,
+            cover_image_url=cover_image_url  # Save URL to database
+        )
+
         db.session.add(new_post)
         db.session.commit()
         return redirect('/')
@@ -58,7 +99,7 @@ def delete(id):
     post = BlogPost.query.get_or_404(id)
     db.session.delete(post)
     db.session.commit()
-    return redirect('/posts')
+    return redirect('/')
 
 @app.route('/posts/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
@@ -69,8 +110,22 @@ def edit(id):
         post.title = request.form['title']
         post.author = request.form['author']
         post.content = request.form['content']
+        cover_image = request.files.get('cover_image')
+        if cover_image:
+            try:
+                filename = f"{uuid.uuid4()}_{secure_filename(cover_image.filename)}"
+                s3.upload_fileobj(
+                    cover_image,
+                    os.getenv('S3_BUCKET_NAME'),
+                    filename,
+                    ExtraArgs={'ContentType': cover_image.content_type}
+                )
+                post.cover_image_url = f"https://{os.getenv('S3_BUCKET_NAME')}.s3.amazonaws.com/{filename}"
+            except Exception as e:
+                return str(e), 500
+
         db.session.commit()
-        return redirect('/posts')
+        return redirect('/')
     else:
         return render_template('edit.html', post=post)
 
